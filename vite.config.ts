85e261b38c2a5de292ee8efd0af84181d1c43af7
robysonfg <robyson@maris.com.br>
2026-03-30 14:16:03 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/controle-de-diesel/',
+    base: '/https://robysonfg.github.io/controle-de-diesel//',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
